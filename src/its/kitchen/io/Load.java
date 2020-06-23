0140e268bc28ad37bb1c0db0fa4d7370ef16a30a
PinPhreek <max.bornhardt@t-online.de>
2020-06-23 12:43:26 +0200

    fixed spelling mistakes and minor issues

--- src/its/kitchen/io/Load.java
+++ src/its/kitchen/io/Load.java
@@ -8,69 +8,61 @@ import its.kitchen.main.Main;
 
 public class Load {
  
-	public static ArrayList<Ingredient> readIngredients(String path) {//Don't care about / and \
+	public static ArrayList<Ingredient> readIngredients(String path) throws FileNotFoundException{//Don't care about / and \
 
 		String s = null, s2 = null /*for some nice switcheroo*/, name = null;
 		boolean inIngredients = false, inHeader = false;
 		int type = 0;
 		ArrayList<Ingredient> data = new ArrayList<>();
-		try {
-			Scanner sc = new Scanner(new File(path));
-			
-			while(sc.hasNextLine()) {
-				if(inIngredients && inHeader) {
-					System.err.println("Mal-formated config-file!\nAbortig.\n");
-					System.exit(-1);
-				}
-				s = sc.nextLine().replace("\t", "").replace(" ", "");
-				if(s.isEmpty())
-					continue;
-				//s = s.toLowerCase();
-				s2 = s.replace("_", " ");
-				s = s2;/*Don't even ask...*/
-				if(s.equalsIgnoreCase("<header>")) {
-					inHeader = true;
-					continue;
-				}
-				if(inHeader) {//Version und so
-					if(s.equalsIgnoreCase("</header>")) {
-						inHeader = false;
-					}
-					else if(s.contains("<version>") && s.contains("</version>")) {
-						Main.data_version = s.replace("<version>", "").replace("</version>", "");
-					}
-					else if(s.equalsIgnoreCase("</header>")) {
-						inHeader = false;
-					}
-					continue;
-				}
-				if(s.contains("<ingredients>")) {
-					inIngredients = true;
-					continue;
-				}
-				if(inIngredients) {
-					if(s.contains("<type")) {
-						type = Ingredient.convertFood(s.split("=")[1].replace(">", ""));
-					}
-					if(s.contains("<name>") && s.contains("</name>")) {
-						name = s.replace("<name>", "").replace("</name>", "");
-						data.add(new Ingredient(name, type));
-					}
-					else if (s.contains("</type>")) {
-						type = 0;
-					}
-					else if(s.contains("</ingredients>")) {
-						inIngredients = false;
-					}
-				}
-				
+		Scanner sc = new Scanner(new File(path));
+		while(sc.hasNextLine()) {
+			if(inIngredients && inHeader) {
+				System.err.println("Mal-formated config-file!\nAbortig.\n");
+				System.exit(-1);
 			}
-			
-			sc.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(-1);
+			s = sc.nextLine().replace("\t", "").replace(" ", "");
+			if(s.isEmpty())
+				continue;
+			//s = s.toLowerCase();
+			s2 = s.replace("_", " ");
+			s = s2;/*Don't even ask...*/
+			if(s.equalsIgnoreCase("<header>")) {
+				inHeader = true;
+				continue;
+			}
+			if(inHeader) {//Version und so
+				if(s.equalsIgnoreCase("</header>")) {
+					inHeader = false;
+				}
+				else if(s.contains("<version>") && s.contains("</version>")) {
+					Main.data_version = s.replace("<version>", "").replace("</version>", "");
+				}
+				else if(s.equalsIgnoreCase("</header>")) {
+					inHeader = false;
+				}
+				continue;
+			}
+			if(s.contains("<ingredients>")) {
+				inIngredients = true;
+				continue;
+			}
+			if(inIngredients) {
+				if(s.contains("<type")) {
+					type = Ingredient.convertFood(s.split("=")[1].replace(">", ""));
+				}
+				if(s.contains("<name>") && s.contains("</name>")) {
+					name = s.replace("<name>", "").replace("</name>", "");
+					data.add(new Ingredient(name, type));
+				}
+				else if (s.contains("</type>")) {
+					type = 0;
+				}
+				else if(s.contains("</ingredients>")) {
+					inIngredients = false;
+				}
+			}
 		}
+		sc.close();
 		return data;
 	}
 	
--- src/its/kitchen/main/Main.java
+++ src/its/kitchen/main/Main.java
@@ -1,5 +1,6 @@
 package its.kitchen.main;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -8,125 +9,145 @@ import its.kitchen.io.Load;
 import its.kitchen.modes.CompletelyRandom;
 import its.kitchen.modes.ParameterRandom;
 
-public class Main {
+public class Main{
 
-	public static String data_version = null;
-	public static String version = "0.1_testing";
-	//public static String dataPath = "ingredients.txt";
-	public static String dataPath = "/home/max/ingredients.txt";
-	
-	public static ArrayList<Ingredient> ingerdients;
-	
-	public static int mode = 0;//1;
-	public static ParameterRandom paramRand;
-	
-	public static boolean verbose = false;
-	
-	public static void main(String[] args) {
-		int i = 0;
-		paramRand = new ParameterRandom();
-		/**
-		 * -f Specifies the data-file. If not present, look in current directory for ingredients.txt.
-		 * -m Specifies the mode, this program should use.
-		 * -v Specifies if the user wants more output.
-		 * -s Specifies the maximum number of steps, the user wants to make.
-		 * -i Specifies the maximum number of ingredients, the user wants to use.
-		 * -u Specifies the units that are used in th program. If not specified the local locale will be used.
-		 * -h displays a help-page.
-		 * */
-		for (i = 0; i < args.length; i++) {
-			if(args[i].equals("-h")) {
-				
-				System.out.println("Kitchen Bravery v." + version + " help.");
-				System.out.printf ("%-2s %s\n", "-f", "Specifies the data-file. If not present, look in current directory for ingredients.txt.");
-				System.out.printf ("%-2s %s\n", "-m", "Specifies the mode, this program should use.");
-				System.out.printf ("%-2s %s\n", "-v", "Enables verbose output");
-				System.out.printf ("%-2s %s\n", "-s", "How much steps you want to do at maximum.");
-				System.out.printf ("%-2s %s\n", "-i", "How many ingredients you want to use.");
-				System.out.printf ("%-2s %s\n", "-u", "What units you use. If not specified, the system-language will be used.");
-				System.out.printf ("%-2s %s\n", "-h", "Displays this help message.");
-				
-				System.exit(0);
-			}
-			else if(args[i].equals("-f")) {
-				if(i+1 >= args.length) {
-					System.err.println("No file specified!\n");
-				}
-				else {
-					dataPath = args[i+1];
-					i++;
-				}
-			}
-			else if(args[i].equals("-m")) {
-				if(i + 1 >= args.length) { //these 2 can't be merged! DON'T EVEN THINK ABOUT IT!
-					System.err.println("No mode specified!\nProceeding with parameter-mode: 5(max) steps, 10(max) ingredients.");
-					mode = MODE_PARAMETER_RANDOM;
-					paramRand.setMaxSteps(5);
-					paramRand.setMaxIngredients(10);
-				}
-				else if (args[i+1].contains("-")){
-					System.err.println("No mode specified!\nProceeding with parameter-mode: 5 steps, 10 ingredients.");
-					mode = MODE_PARAMETER_RANDOM;
-					paramRand.setMaxSteps(5);
-					paramRand.setMaxIngredients(10);
-				}
-				else {
-					if (args[i+1].equalsIgnoreCase("random")) mode = MODE_COMPLETELY_RANDOM;
-					else if (args[i+1].equalsIgnoreCase("parameter")) mode = MODE_PARAMETER_RANDOM;
-					else if (args[i+1].equalsIgnoreCase("lying") && i + 2>= args.length){
-						if (args[i+2].equalsIgnoreCase("around")) mode = MODE_LYING_AROUND; //we don't want an index out of bounds
-						else{
-							mode = MODE_PARAMETER_RANDOM;
-							paramRand.setMaxSteps(5);
-							paramRand.setMaxIngredients(10);
-						}
-					}
-				}
-			}
-			else if(args[i].equals("-v")) {
-				verbose = true;
-			}
-			else if(args[i].equals("-s")) {
-				if (i+1 >= args.length) {
-					System.err.println("No number of steps specified!\nGoing with maximum 5 steps.");
-					paramRand.setMaxSteps(5);
-					continue;
-				}
-				else if (args[i+1].contains("-")){
-					System.err.println("No number of steps specified!\nGoing with maximum 5 steps.");
-					paramRand.setMaxSteps(5);
-					continue;
-				}
-				try {
-					paramRand.setMaxIngredients(Integer.valueOf(args[i + 2]));
-				}catch(Exception e){
-					System.err.println(args[i+2] + " is not a number!\nProceeding with 5 steps.");
-					paramRand.setMaxSteps(5);
-				}
-			}
-			else if (args[i].equals("-u")){
-				if (i+1 >= args.length)
-					System.err.println("No language specified!\nGoing with " + Locale.getDefault().getLanguage() + ".");
-				else{
-					/*HIER CODE EINFÜGEN*/
-				}
-			}
-			System.out.println(args[i]);
-		}
-		ingerdients = Load.readIngredients(dataPath);
-		for (i = 0; i < ingerdients.size(); i++) {
-			ingerdients.get(i).displayIngredient();
-		}
-		if(mode == MODE_COMPLETELY_RANDOM) {
-		
-			CompletelyRandom.generateRecipe();
-			
-		}
-		
-	}
-	
-	public static final int MODE_COMPLETELY_RANDOM = 0;
-	public static final int MODE_PARAMETER_RANDOM  = 1;
-	public static final int MODE_LYING_AROUND      = 2;
-	
+    public static String data_version = null;
+    public static String version = "0.1_testing";
+    //public static String dataPath = "ingredients.txt";
+    public static String dataPath = "/home/max/ingredients.txt";
+
+    public static ArrayList<Ingredient> ingerdients;
+
+    public static int mode = 0;//1;
+    public static ParameterRandom paramRand;
+
+    public static boolean verbose = false;
+
+    public static void main(String[] args){
+        int i = 0;
+        paramRand = new ParameterRandom();
+        /**
+         * -f Specifies the data-file. If not present, look in current directory for ingredients.txt.
+         * -m Specifies the mode, this program should use.
+         * -v Specifies if the user wants more output.
+         * -s Specifies the maximum number of steps, the user wants to make.
+         * -i Specifies the maximum number of ingredients, the user wants to use.
+         * -u Specifies the units that are used in th program. If not specified the local locale will be used.
+         * -h displays a help-page.
+         * */
+        for (i = 0; i < args.length; i++){
+            if(args[i].equals("-h")){
+
+                System.out.println("Kitchen Bravery v." + version + " help.");
+                System.out.printf("%-2s %s\n", "-f", "Specifies the data-file. If not present, look in current directory for ingredients.txt.");
+                System.out.printf("%-2s %s\n", "-m", "Specifies the mode, this program should use.");
+                System.out.printf("%-2s %s\n", "-v", "Enables verbose output");
+                System.out.printf("%-2s %s\n", "-s", "How much steps you want to do at maximum.");
+                System.out.printf("%-2s %s\n", "-i", "How many ingredients you want to use.");
+                System.out.printf("%-2s %s\n", "-u", "What units you use. If not specified, the system-language will be used.");
+                System.out.printf("%-2s %s\n", "-h", "Displays this help message.");
+
+                System.exit(0);
+            }else if(args[i].equals("-f")){
+                if(i + 1 >= args.length){
+                    System.err.println("No file specified!\nUsing ingredients.txt");
+                }else if(args[i + 1].contains("-")) System.err.println("No file specified!\nUsing ingredients.txt");
+                else{
+                    dataPath = args[i + 1];
+                    i++;
+                }
+            }else if(args[i].equals("-m")){
+                if(i + 1 >= args.length){ //these 2 can't be merged! DON'T EVEN THINK ABOUT IT!
+                    System.err.println("No mode specified!\nProceeding with parameter-mode: 5(max) steps, 10(max) ingredients.");
+                    mode = MODE_PARAMETER_RANDOM;
+                    paramRand.setMaxSteps(5);
+                    paramRand.setMaxIngredients(10);
+                }else if(args[i + 1].contains("-")){
+                    System.err.println("No mode specified!\nProceeding with parameter-mode: 5 steps, 10 ingredients.");
+                    mode = MODE_PARAMETER_RANDOM;
+                    paramRand.setMaxSteps(5);
+                    paramRand.setMaxIngredients(10);
+                }else{
+                    if(args[i + 1].equalsIgnoreCase("random")){
+                        mode = MODE_COMPLETELY_RANDOM;
+                        i++;
+                    }else if(args[i + 1].equalsIgnoreCase("parameter")){
+                        mode = MODE_PARAMETER_RANDOM;
+                        i++;
+                    }else if(args[i + 1].equalsIgnoreCase("lying") && i + 2 >= args.length){
+                        if(args[i + 2].equalsIgnoreCase("around"))
+                            mode = MODE_LYING_AROUND; //we don't want an index out of bounds
+                        else{
+                            mode = MODE_PARAMETER_RANDOM;
+                            paramRand.setMaxSteps(5);
+                            paramRand.setMaxIngredients(10);
+                        }
+                        i += 2;
+                    }
+                }
+            }else if(args[i].equals("-v")){
+                verbose = true;
+            }else if(args[i].equals("-s")){
+                if(i + 1 >= args.length){
+                    System.err.println("No number of steps specified!\nGoing with maximum 5 steps.");
+                    paramRand.setMaxSteps(5);
+                    continue;
+                }else if(args[i + 1].contains("-")){
+                    System.err.println("No number of steps specified!\nGoing with maximum 5 steps.");
+                    paramRand.setMaxSteps(5);
+                    continue;
+                }
+                try{
+                    paramRand.setMaxSteps(Integer.valueOf(args[i + 1]));
+                } catch (Exception e){
+                    System.err.println(args[i + 1] + " is not a number!\nProceeding with 5 steps.");
+                    paramRand.setMaxSteps(5);
+                }
+                i++;
+            }else if(args[i].equals("-i")){
+                if(i + 1 >= args.length){
+                    System.err.println("No maximum number of ingredients specified!\nProceeding with 10 ingredients.");
+                    paramRand.setMaxIngredients(10);
+                    continue;
+                }else if(args[i + 1].contains("-")){
+                    System.err.println("No maximum number of ingredients specified!\nProceeding with 10 ingredients.");
+                    paramRand.setMaxIngredients(10);
+                    continue;
+                }
+                try{
+                    paramRand.setMaxIngredients(Integer.valueOf(args[i+1]));
+                }catch (Exception e){
+                    System.err.println(args[i+1] + " is not a number!\nProceeding with 10 ingredients.");
+                    paramRand.setMaxIngredients(10);
+                }
+                i++;
+            }else if(args[i].equals("-u")){
+                if(i + 1 >= args.length)
+                    System.err.println("No language specified!\nGoing with " + Locale.getDefault().getLanguage() + ".");
+                else{
+                    /*HIER CODE EINFÜGEN*/
+                }
+            }
+            System.out.println(args[i]);
+        }
+        try{
+            ingerdients = Load.readIngredients(dataPath);
+        } catch (FileNotFoundException e){
+
+        }
+        for (i = 0; i < ingerdients.size(); i++){
+            ingerdients.get(i).displayIngredient();
+        }
+        if(mode == MODE_COMPLETELY_RANDOM){
+
+            CompletelyRandom.generateRecipe();
+
+        }
+
+    }
+
+    public static final int MODE_COMPLETELY_RANDOM = 0;
+    public static final int MODE_PARAMETER_RANDOM = 1;
+    public static final int MODE_LYING_AROUND = 2;
+
 }
